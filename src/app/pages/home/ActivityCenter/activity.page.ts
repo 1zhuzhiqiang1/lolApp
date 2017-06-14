7545ffef623d8eeef5d7b01b639af48ae65f505a
zhuzhiqiang <mzhuzhiqiang@163.com>
2017-06-14 18:58:08 +0800

    0614

--- src/app/pages/home/ActivityCenter/activity.page.ts
+++ src/app/pages/home/ActivityCenter/activity.page.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Http} from '@angular/http';
+import {Platform} from 'ionic-angular';
 
 import {Config} from '../../../../app/Utils/Config';
 
@@ -9,21 +10,35 @@ declare let RongCloudLibPlugin: any;
   templateUrl: 'activity.page.html'
 })
 export class ActivityPage {
+  news: any;
 
-  constructor(private http: Http) {
+  constructor(private http: Http, public plt: Platform) {
   }
 
   ionViewDidLoad() {
-    RongCloudLibPlugin.connect({
-      token: "sQKWLNsD/DbNJ0oAdB6+BscyfAwJjopBNJrfR2IJPGvmxvT57NJ3cr5wKf2a1fFWqSdLTDJ+D4pLS09/nD/hGCnkLPT1KLVZ"
-    }, function (ret, err) {
-      console.log(ret);
-      if (ret.status == 'success') {
-        console.log("登录的用户是：" + ret.result.userId);
-      } else {
-        console.log("链接服务器失败");
-      }
-    });
+    // 链接服务器
+    if (this.plt.is("ios") || this.plt.is("android")) {
+      RongCloudLibPlugin.connect({
+        token: "sQKWLNsD/DbNJ0oAdB6+BscyfAwJjopBNJrfR2IJPGvmxvT57NJ3cr5wKf2a1fFWqSdLTDJ+D4pLS09/nD/hGCnkLPT1KLVZ"
+      }, function (ret, err) {
+        console.log(ret);
+        if (ret.status == 'success') {
+          console.log("登录的用户是：" + ret.result.userId);
+          RongCloudLibPlugin.joinChatRoom({
+            chatRoomId: '666666',
+            defMessageCount: 20
+          }, function (ret, err) {
+            if (ret.status == 'success')
+              console.log(JSON.stringify(ret.status));
+            else
+              alert(err.code);
+          })
+        } else {
+          console.log("链接服务器失败");
+        }
+      });
+    }
   }
+
 
 }
